73f392905dc8a469ae790e5f902463cae8325193
NoseferatuWKF <wkf2584@gmail.com>
2021-06-19 21:10:13 +0800

    added node backend and renamed angular app and updated .gitignore

---
+++ apps/node-js-backend-api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'node-js-backend-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/node-js-backend-api',
+};
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/react-base-to-do-app',
+    '<rootDir>/apps/node-js-backend-api',
+    '<rootDir>/apps/angular-base-space-x-graphql',
+  ],
+};
