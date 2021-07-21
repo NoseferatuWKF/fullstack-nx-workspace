f0a4cff9fe66ffe2a382f37a15eb75013c653ef3
NoseferatuWKF <wkf2584@gmail.com>
2021-07-21 20:41:17 +0800

    testing CI

--- apps/portfolio/src/app/resume-body/resume-body.tsx
+++ apps/portfolio/src/app/resume-body/resume-body.tsx
@@ -9,25 +9,33 @@ export function ResumeBody() {
               <div className="message-header">
                 <p>Wavelet Solutions Sdn Bhd</p>
               </div>
-              <div className="message-body">...</div>
+              <div className="message-body">
+              Assistant Manager | Software Developer
+              </div>
             </article>
             <article className="message is-link">
               <div className="message-header">
                 <p>Prudential Assurance Malaysia Berhad</p>
               </div>
-              <div className="message-body">...</div>
+              <div className="message-body">
+              Executive Management Trainee
+              </div>
             </article>
             <article className="message is-link">
               <div className="message-header">
                 <p>BMW Malaysia</p>
               </div>
-              <div className="message-body">...</div>
+              <div className="message-body">
+              Apprentice
+              </div>
             </article>
             <article className="message is-link">
               <div className="message-header">
                 <p>UiTM</p>
               </div>
-              <div className="message-body">...</div>
+              <div className="message-body">
+              Assistant Engineer
+              </div>
             </article>
           </article>
         </section>
